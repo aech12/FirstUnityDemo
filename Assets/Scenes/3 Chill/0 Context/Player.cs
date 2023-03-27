463219560c231f5b112516d0c2f0b5ee70b3a319
aech12 <aech-12@hotmail.com>
2023-03-27 02:47:41 -0300

    adjusted movement, created camera
    camera follows player, movement and camera work as intended on /chill

--- Assets/Scenes/3 Chill/0 Context/Player.cs
+++ Assets/Scenes/3 Chill/0 Context/Player.cs
@@ -14,7 +14,15 @@ public class PlayerController : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            // this is regular move
+            // moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+            // this is move for isometric
+            float angle = -45f * Mathf.Deg2Rad;
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            moveDirection = new Vector3(horizontal * Mathf.Cos(angle) - vertical * Mathf.Sin(angle), 0.0f, horizontal * Mathf.Sin(angle) + vertical * Mathf.Cos(angle));
+
             moveDirection *= speed;
 
             if (Input.GetButton("Jump"))
